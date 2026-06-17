661b842275cb483593d1ddfff69101444731b9d9
Rudhransh Dhiman <rudhranshpvtt@gmail.com>
2026-06-17 16:40:57 +0530

    delete task completed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,15 @@
 import "./Card.css";
 import { useDraggable } from '@dnd-kit/core'
 import { CSS } from '@dnd-kit/utilities'
+import useBoardStore from "../../store/useBoardStore";
 
 const Card = ({ task, columnId }) => {
+    const deleteTask = useBoardStore((state) => state.deleteTask)
     const { attributes, listeners, setNodeRef, transform } = useDraggable({ id: task.id })
+    const handleDelete = (e) => {
+        e.stopPropagation();
+        deleteTask(task.id, columnId);
+    }
     return (
         <div className="card" ref={setNodeRef}
             {...attributes}
@@ -12,6 +18,8 @@ const Card = ({ task, columnId }) => {
             <div className="card__header">
                 <span className="card__label">{task.label}</span>
                 <span className="card__priority">{task.priority}</span>
+                <button className="card__delete-btn" onClick={handleDelete} onPointerDown={(e) => e.stopPropagation()}
+>X</button>
             </div>
             <h4 className="card__title">{task.title}</h4>
             <p className="card__description">{task.description}</p>
